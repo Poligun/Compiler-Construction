ff15819d9a7f4a932efec00d72c6677bd8a42a41
ZhaoYuhan <zhaoyuhan@ZhaoYuhandeMacBook-Pro.local>
2015-10-24 21:44:28 -0400

    Added spaces between terminals.

--- project2/Pr2Yuhan.hx
+++ project2/Pr2Yuhan.hx
@@ -6,31 +6,6 @@ module yuhan.project2.Pr2Yuhan
     | "/*"([^*]|"*"[^/]|\r?\n)*"*/"
     ;
 
-    /* If this separator weren't defined but embedded instead,
-     * hacs wouldn't be able to recognized statements like
-     * 'x = {};'
-     */
-    token Separator | ";";
-
-    // Reserved words
-    token Var | "var";
-    token Function | "function";
-    token Interface | "interface";
-
-    token If     | "if";
-    token Else   | "else";
-    token Return | "return";
-
-    token While | "while";
-
-    token Null      | "null";
-    token Undefined | "undefined";
-
-    token BooleanType | boolean;
-    token NumberType  | number;
-    token StringType  | string;
-    token VoidType    | void;
-
     token Identifier | [a-zA-Z$_][a-zA-Z0-9$_]*;
     token Integer    | [0-9]+;
 
@@ -43,144 +18,144 @@ module yuhan.project2.Pr2Yuhan
     token fragment Escape | \\([\'\"\\nt]|(\r?\n)|(x⟨HexDigit⟩⟨HexDigit⟩));
     
     main sort Program
-    | ⟦⟨StatementSequence⟩⟧
+    | ⟦ ⟨StatementSequence⟩ ⟧
     ;
 
     sort Expression
     // Primitives have the highest precedence
-    | ⟦⟨AnonymousFunction⟩⟧ @19
-    | ⟦⟨Identifier⟩⟧ @19
-    | ⟦⟨Integer⟩⟧ @19
-    | ⟦⟨String⟩⟧ @19
-    | ⟦⟨Object⟩⟧ @19
-    | ⟦⟨Undefined⟩⟧ @19
-    | sugar ⟦(⟨Expression #⟩)⟧ @19 → #
+    | ⟦ ⟨AnonymousFunction⟩ ⟧ @19
+    | ⟦ ⟨Identifier⟩ ⟧ @19
+    | ⟦ ⟨Integer⟩ ⟧ @19
+    | ⟦ ⟨String⟩ ⟧ @19
+    | ⟦ ⟨Object⟩ ⟧ @19
+    | ⟦ undefined ⟧ @19
+    | sugar ⟦ ( ⟨Expression #⟩ ) ⟧ @19 → #
     // Function call and Member access
-    | ⟦⟨Expression @18⟩(⟨ExpressionSequence⟩)⟧ @18
-    | ⟦⟨Expression @18⟩.⟨Expression @19⟩⟧ @18
+    | ⟦ ⟨Expression @18⟩ ( ⟨ExpressionSequence⟩ ) ⟧ @18
+    | ⟦ ⟨Expression @18⟩ . ⟨Expression @19⟩ ⟧ @18
     // Unary Operators
-    | ⟦!⟨Expression @15⟩⟧ @15
-    | ⟦~⟨Expression @15⟩⟧ @15
-    | ⟦-⟨Expression @15⟩⟧ @15
-    | ⟦+⟨Expression @15⟩⟧ @15
+    | ⟦ ! ⟨Expression @15⟩ ⟧ @15
+    | ⟦ ~ ⟨Expression @15⟩ ⟧ @15
+    | ⟦ - ⟨Expression @15⟩ ⟧ @15
+    | ⟦ + ⟨Expression @15⟩ ⟧ @15
     // Arithmetic Operators
-    | ⟦⟨Expression @14⟩*⟨Expression @15⟩⟧ @14
-    | ⟦⟨Expression @14⟩/⟨Expression @15⟩⟧ @14
-    | ⟦⟨Expression @14⟩%⟨Expression @15⟩⟧ @14
+    | ⟦ ⟨Expression @14⟩ * ⟨Expression @15⟩ ⟧ @14
+    | ⟦ ⟨Expression @14⟩ / ⟨Expression @15⟩ ⟧ @14
+    | ⟦ ⟨Expression @14⟩ % ⟨Expression @15⟩ ⟧ @14
     // Arithmetic Operators
-    | ⟦⟨Expression @13⟩+⟨Expression @14⟩⟧ @13
-    | ⟦⟨Expression @13⟩-⟨Expression @14⟩⟧ @13
+    | ⟦ ⟨Expression @13⟩ + ⟨Expression @14⟩ ⟧ @13
+    | ⟦ ⟨Expression @13⟩ - ⟨Expression @14⟩ ⟧ @13
     // Relational Operators
-    | ⟦⟨Expression @11⟩<⟨Expression @12⟩⟧ @11
-    | ⟦⟨Expression @11⟩>⟨Expression @12⟩⟧ @11
-    | ⟦⟨Expression @11⟩<=⟨Expression @12⟩⟧ @11
-    | ⟦⟨Expression @11⟩>=⟨Expression @12⟩⟧ @11
-    | ⟦⟨Expression @10⟩==⟨Expression @11⟩⟧ @10
-    | ⟦⟨Expression @10⟩!=⟨Expression @11⟩⟧ @10
+    | ⟦ ⟨Expression @11⟩ <  ⟨Expression @12⟩ ⟧ @11
+    | ⟦ ⟨Expression @11⟩ >  ⟨Expression @12⟩ ⟧ @11
+    | ⟦ ⟨Expression @11⟩ <= ⟨Expression @12⟩ ⟧ @11
+    | ⟦ ⟨Expression @11⟩ >= ⟨Expression @12⟩ ⟧ @11
+    | ⟦ ⟨Expression @10⟩ == ⟨Expression @11⟩ ⟧ @10
+    | ⟦ ⟨Expression @10⟩ != ⟨Expression @11⟩ ⟧ @10
     // Bitwise Operators
-    | ⟦⟨Expression @9⟩&⟨Expression @10⟩⟧ @9
-    | ⟦⟨Expression @8⟩^⟨Expression @9⟩⟧ @8
-    | ⟦⟨Expression @7⟩|⟨Expression @8⟩⟧ @7
+    | ⟦ ⟨Expression @9⟩ & ⟨Expression @10⟩ ⟧ @9
+    | ⟦ ⟨Expression @8⟩ ^ ⟨Expression @9⟩ ⟧ @8
+    | ⟦ ⟨Expression @7⟩ | ⟨Expression @8⟩ ⟧ @7
     // Logical Operators
-    | ⟦⟨Expression @6⟩&&⟨Expression @7⟩⟧ @6
-    | ⟦⟨Expression @5⟩||⟨Expression @6⟩⟧ @5
+    | ⟦ ⟨Expression @6⟩ && ⟨Expression @7⟩ ⟧ @6
+    | ⟦ ⟨Expression @5⟩ || ⟨Expression @6⟩ ⟧ @5
     // Ternary Operator
-    | ⟦⟨Expression @5⟩?⟨Expression⟩:⟨Expression @4⟩⟧ @4
+    | ⟦ ⟨Expression @5⟩ ? ⟨Expression @2⟩ : ⟨Expression @2⟩ ⟧ @4
     // Assignment Operators
-    | ⟦⟨Expression @4⟩=⟨Expression @3⟩⟧ @3
-    | ⟦⟨Expression @4⟩+=⟨Expression @3⟩⟧ @3
-    | ⟦⟨Expression @4⟩-=⟨Expression @3⟩⟧ @3
-    | ⟦⟨Expression @4⟩*=⟨Expression @3⟩⟧ @3
-    | ⟦⟨Expression @4⟩/=⟨Expression @3⟩⟧ @3
-    | ⟦⟨Expression @4⟩&=⟨Expression @3⟩⟧ @3
-    | ⟦⟨Expression @4⟩^=⟨Expression @3⟩⟧ @3
-    | ⟦⟨Expression @4⟩|=⟨Expression @3⟩⟧ @3
+    | ⟦ ⟨Expression @4⟩ =  ⟨Expression @3⟩ ⟧ @3
+    | ⟦ ⟨Expression @4⟩ += ⟨Expression @3⟩ ⟧ @3
+    | ⟦ ⟨Expression @4⟩ -= ⟨Expression @3⟩ ⟧ @3
+    | ⟦ ⟨Expression @4⟩ *= ⟨Expression @3⟩ ⟧ @3
+    | ⟦ ⟨Expression @4⟩ /= ⟨Expression @3⟩ ⟧ @3
+    | ⟦ ⟨Expression @4⟩ &= ⟨Expression @3⟩ ⟧ @3
+    | ⟦ ⟨Expression @4⟩ ^= ⟨Expression @3⟩ ⟧ @3
+    | ⟦ ⟨Expression @4⟩ |= ⟨Expression @3⟩ ⟧ @3
     // Comma Operator
-    | ⟦⟨Expression @1⟩,⟨Expression @2⟩⟧ @1
+    | ⟦ ⟨Expression @1⟩ , ⟨Expression @2⟩ ⟧ @1
     ;
     
     sort ExpressionSequence
     | ⟦⟧ @1
-    | ⟦⟨Expression @2⟩⟧ @2
-    | ⟦⟨Expression @2⟩,⟨ExpressionSequence @2⟩⟧ @2
+    | ⟦ ⟨Expression @2⟩ ⟧ @2
+    | ⟦ ⟨Expression @2⟩ , ⟨ExpressionSequence @2⟩ ⟧ @2
     ;
 
     sort Object
-    | ⟦⟨Null⟩⟧
-    | ⟦{⟨NameValuePairSequence⟩}⟧
+    | ⟦ null ⟧
+    | ⟦ { ⟨NameValuePairSequence⟩ } ⟧
     ;
 
     sort NameValuePair
-    | ⟦⟨Identifier⟩:⟨Expression @2⟩⟧
-    | ⟦⟨String⟩:⟨Expression @2⟩⟧
+    | ⟦ ⟨Identifier⟩ : ⟨Expression @2⟩ ⟧
+    | ⟦ ⟨String⟩ : ⟨Expression @2⟩ ⟧
     ;
 
     sort NameValuePairSequence
     | ⟦⟧
-    | ⟦⟨NameValuePair⟩⟧
-    | ⟦⟨NameValuePair⟩,⟨NameValuePairSequence⟩⟧
+    | ⟦ ⟨NameValuePair⟩ ⟧
+    | ⟦ ⟨NameValuePair⟩ , ⟨NameValuePairSequence⟩ ⟧
     ;
     
     sort Type
-    | ⟦⟨BooleanType⟩⟧
-    | ⟦⟨NumberType⟩⟧
-    | ⟦⟨StringType⟩⟧
-    | ⟦⟨VoidType⟩⟧
-    | ⟦⟨Identifier⟩⟧
-    | ⟦(⟨TypeSequence⟩)=>⟨Type⟩⟧
-    | ⟦{⟨NameTypePairSequence⟩}⟧
+    | ⟦ boolean ⟧
+    | ⟦ number ⟧
+    | ⟦ string ⟧
+    | ⟦ void ⟧
+    | ⟦ ⟨Identifier⟩ ⟧
+    | ⟦ ( ⟨TypeSequence⟩ ) => ⟨Type⟩ ⟧
+    | ⟦ { ⟨NameTypePairSequence⟩ } ⟧
     ;
 
     sort TypeSequence
     | ⟦⟧ @1
-    | ⟦⟨Type⟩⟧ @2
-    | ⟦⟨Type⟩,⟨TypeSequence @2⟩⟧ @2
+    | ⟦ ⟨Type⟩ ⟧ @2
+    | ⟦ ⟨Type⟩ , ⟨TypeSequence @2⟩ ⟧ @2
     ;
 
     sort NameTypePair
-    | ⟦⟨Identifier⟩:⟨Type⟩⟧
+    | ⟦ ⟨Identifier⟩ : ⟨Type⟩ ⟧
     ;
 
     sort NameTypePairSequence
     | ⟦⟧ @1
-    | ⟦⟨NameTypePair⟩⟧ @2
-    | ⟦⟨NameTypePair⟩,⟨NameTypePairSequence⟩⟧ @2
+    | ⟦ ⟨NameTypePair⟩ ⟧ @2
+    | ⟦ ⟨NameTypePair⟩ , ⟨NameTypePairSequence⟩ ⟧ @2
     ;
 
     sort InterfaceMember
-    | ⟦⟨Identifier⟩:⟨Type⟩⟨Separator⟩⟧
-    | ⟦⟨Identifier⟩⟨CallSignature⟩{⟨StatementSequence⟩}⟨Separator⟩⟧
+    | ⟦ ⟨Identifier⟩ : ⟨Type⟩ ; ⟧
+    | ⟦ ⟨Identifier⟩ ⟨CallSignature⟩ { ⟨StatementSequence⟩ } ; ⟧
     ;
 
     sort InterfaceMemberSequence
     | ⟦⟧
-    | ⟦⟨InterfaceMember⟩⟨InterfaceMemberSequence⟩⟧
+    | ⟦ ⟨InterfaceMember⟩ ⟨InterfaceMemberSequence⟩ ⟧
     ;
 
     sort AnonymousFunction
-    | ⟦⟨Function⟩⟨CallSignature⟩{⟨StatementSequence⟩}⟧
+    | ⟦ function ⟨CallSignature⟩ { ⟨StatementSequence⟩ } ⟧
     ;
 
     sort CallSignature
-    | ⟦(⟨NameTypePairSequence⟩):⟨Type⟩⟧
+    | ⟦ ( ⟨NameTypePairSequence⟩ ) : ⟨Type⟩ ⟧
     ;
 
     sort Statement
-    | ⟦⟨Var⟩⟨NameTypePairSequence⟩⟨Separator⟩⟧
-    | ⟦⟨Function⟩⟨Identifier⟩⟨CallSignature⟩{⟨StatementSequence⟩}⟧
-    | ⟦⟨Interface⟩⟨Identifier⟩{⟨InterfaceMemberSequence⟩}⟧
-    | ⟦⟨Expression⟩⟨Separator⟩⟧
-    | ⟦⟨Separator⟩⟧
-    | ⟦{⟨StatementSequence⟩}⟧
-    | ⟦⟨If⟩(⟨Expression⟩)⟨Statement⟩⟨Else⟩⟨Statement⟩⟧
-    | ⟦⟨If⟩(⟨Expression⟩)⟨Statement⟩⟧
-    | ⟦⟨While⟩(⟨Expression⟩)⟨Statement⟩⟧
-    | ⟦⟨Return⟩⟨Expression⟩⟨Separator⟩⟧
-    | ⟦⟨Return⟩⟨Separator⟩⟧
+    | ⟦ var ⟨NameTypePairSequence⟩ ; ⟧
+    | ⟦ function ⟨Identifier⟩ ⟨CallSignature⟩ { ⟨StatementSequence⟩ } ⟧
+    | ⟦ interface ⟨Identifier⟩ { ⟨InterfaceMemberSequence⟩ } ⟧
+    | ⟦ ⟨Expression⟩ ; ⟧
+    | ⟦ ; ⟧
+    | ⟦ { ⟨StatementSequence⟩ } ⟧
+    | ⟦ if ( ⟨Expression⟩ ) ⟨Statement⟩ else ⟨Statement⟩ ⟧
+    | ⟦ if ( ⟨Expression⟩ ) ⟨Statement⟩ ⟧
+    | ⟦ while ( ⟨Expression⟩ ) ⟨Statement⟩ ⟧
+    | ⟦ return ⟨Expression⟩ ; ⟧
+    | ⟦ return ; ⟧
     ;
 
     sort StatementSequence
     | ⟦⟧
-    | ⟦⟨Statement⟩⟨StatementSequence⟩⟧
+    | ⟦ ⟨Statement⟩ ⟨StatementSequence⟩ ⟧
     ;
 }
